7a2f170a54119e402667e52f53e40363e3f8d3c1
Lucas Fernandes <lucas_matheus22@hotmail.com>
2023-01-23 20:07:35 -0300

    Input de dados

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from 'react-native'
+import { Text, TextInput, View } from 'react-native'
 
 import { styles } from './styles'
 
@@ -7,6 +7,11 @@ export default function Home() {
     <View style={styles.container}>
       <Text style={styles.eventName}>Nome do Evento</Text>
       <Text style={styles.eventDate}>Segunda, 23 de Janeiro de 2023</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Nome do participante"
+        placeholderTextColor="#6B6B6B"
+      />
     </View>
   );
 }
