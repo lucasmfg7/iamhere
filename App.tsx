eb4f4a2f6269a68c2c62a9e11371dc187c15c133
Lucas Fernandes <lucas_matheus22@hotmail.com>
2023-01-23 19:46:35 -0300

    Organizando o projeto

--- App.tsx
+++ App.tsx
@@ -1,25 +1,5 @@
-import { StyleSheet, Text, View } from 'react-native'
+import Home from './src/screens/Home'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.eventName}>Nome do Evento</Text>
-      <Text style={styles.eventDate}>Segunda, 23 de Janeiro de 2023</Text>
-    </View>
-  );
+  return <Home />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#131016",
-    padding: 24,
-  },
-  eventName: {
-    color: "#FDFCFE",
-    fontSize: 24,
-    fontWeight: "bold",
-    marginTop: 48,
-  },
-  eventDate: { color: "#6B6B6B", fontSize: 16 },
-});
---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native'
+
+import { styles } from './styles'
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.eventName}>Nome do Evento</Text>
+      <Text style={styles.eventDate}>Segunda, 23 de Janeiro de 2023</Text>
+    </View>
+  );
+}
