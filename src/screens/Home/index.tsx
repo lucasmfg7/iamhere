a21000fbac517f962acd8bfbf7209e012e4d6faf
Lucas Fernandes <lucas_matheus22@hotmail.com>
2023-01-23 20:13:12 -0300

    Botão de Adicionar

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,8 +1,12 @@
-import { Text, TextInput, View } from 'react-native'
+import { Text, TextInput, TouchableOpacity, View } from 'react-native'
 
 import { styles } from './styles'
 
 export default function Home() {
+  function handleParticipantAdd() {
+    console.log("Adicionar participante");
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>Nome do Evento</Text>
@@ -12,6 +16,10 @@ export default function Home() {
         placeholder="Nome do participante"
         placeholderTextColor="#6B6B6B"
       />
+
+      <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+        <Text style={styles.buttonText}>+</Text>
+      </TouchableOpacity>
     </View>
   );
 }
